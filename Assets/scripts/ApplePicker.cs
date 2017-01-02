3729591017355ebbd58ba78b824b3b63f394dcfd
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-01-02 17:23:40 +0000

    Basket movable and can collect apples

---
+++ Assets/scripts/ApplePicker.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[AddComponentMenu ("Vistage/ApplePicker")]
+public class ApplePicker : MonoBehaviour {
+
+	public GameObject basketPrefab;
+	public int numBaskets;
+	public float basketBottomY;
+	public float basketSpacingY;
+
+	private void Start ()
+	{
+		for (int i = 0; i < numBaskets; i++)
+		{
+			GameObject basketGO = Instantiate (basketPrefab) as GameObject;
+			Vector3 pos = Vector3.zero;
+			pos.y = basketBottomY + (basketSpacingY * i);
+			basketGO.transform.position = pos;
+		}
+	}
+}
---
+++ Assets/scripts/Basket.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[AddComponentMenu ("Vistage/Basket")]
+public class Basket : MonoBehaviour {
+
+	private void Update ()
+	{
+		Move ();
+	}
+
+	private void Move ()
+	{
+		Vector3 mousePos2D = Input.mousePosition;
+		mousePos2D.z = -Camera.main.transform.position.z;
+
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint (mousePos2D);
+
+		Vector3 pos = transform.position;
+		pos.x = mousePos3D.x;
+		transform.position = pos;
+	}
+
+	private void OnCollisionEnter (Collision other)
+	{
+		GameObject collided = other.gameObject;
+
+		if (collided.tag == "apple")
+		{
+			Destroy(collided.gameObject);
+		}
+	}
+}
