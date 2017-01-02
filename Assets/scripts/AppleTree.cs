723229c27526d10a36c60ff1c8cc90fb7a7f4c53
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-01-02 15:54:30 +0000

    Apple tree moves left to right and back

---
+++ Assets/scripts/AppleTree.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+[AddComponentMenu ("Vistage/AppleTree")]
+public class AppleTree : MonoBehaviour {
+
+	public GameObject applePrefab;
+
+	public float moveSpeed;
+	public float leftRightEdge;
+	public float chanceToChangeDir;
+	public float timeBetweenAppleDrops;
+
+	private Vector3 pos;
+
+	private void Start ()
+	{
+		// Begin apple dropping
+	}
+
+	private void Update ()
+	{
+		Move();
+		ChangeDirection();
+	}
+
+	private void Move ()
+	{
+		pos = transform.position;
+		pos.x += moveSpeed * Time.deltaTime;
+		transform.position = pos;
+	}
+
+	private void ChangeDirection ()
+	{
+		if (pos.x < -leftRightEdge)
+		{
+			moveSpeed = Mathf.Abs(moveSpeed);
+		}
+		if (pos.x > leftRightEdge)
+		{
+			moveSpeed = -Mathf.Abs(moveSpeed);
+		}
+	}
+}
