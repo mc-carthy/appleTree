2c8cafb5572a8b14aef0c0737bbfff77d263be55
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-01-02 16:34:51 +0000

    Apples are destroyed after falling set distance

---
+++ Assets/scripts/Apple.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[AddComponentMenu ("Vistage/Apple")]
+public class Apple : MonoBehaviour {
+
+	private static float bottomY = -20f;
+
+	private void Update ()
+	{
+		if (transform.position.y < bottomY)
+		{
+			Destroy (gameObject);
+		}
+	}
+}
